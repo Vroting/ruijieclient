84d6e52cfc417c384af523091b89339886b69bdd
sthots <sthots@gmail.com>
2009-06-04 12:27:23 +0000

    First commit.

---
+++ test/authentication.c
@@ -0,0 +1,15 @@
+#include "authentication.h"
+
+
+void auth()
+{
+    g_message("//TODO auth");
+}
+void disauth()
+{
+    g_message("//TODO disauth");
+}
+void get_server_msg()
+{
+    g_message("//TODO get_server_msg");
+}
---
+++ test/dbus_monitor.h
@@ -0,0 +1,10 @@
+#ifndef DBUS_MONITOR_H
+#define DBUS_MONITOR_H
+
+#include "global.h"
+
+void init_dbus();
+
+
+
+#endif /*DBUS_MONITOR_H*/
