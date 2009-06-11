85469f4ea5a2aab7510bfb66e72c8dae9c016f8a
gonghan1989 <gonghan1989@79aabc48-1140-11de-aa17-9100d24c02a7>
2009-06-11 17:03:41 +0000

    1. add support for retrieving default gateway automatically. And, the retrieving result will be printed on the screen if DEBUG is defined.

--- trunk/src/ruijieclient.c
+++ trunk/src/ruijieclient.c
@@ -28,9 +28,7 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
-#include <ctype.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
+
 #include "ruijieclient.h"
 #include "sendpacket.h"
 #include "myerr.h"
